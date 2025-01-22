6aacb03ecfe278e3d70c71e9274ec409618ae99e
Sophia Stock <2898955s@student.gla.ac.uk>
2025-01-22 12:06:30 +0000

    Ch4 Exercise Passed

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunch, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     
     # Return a rendered response to send to the client.
     # We make sue of the shortcut function to make our lives easier.
@@ -15,6 +15,7 @@ def index(request):
 
 #Excersis 3: Added the <a... part and the about part
 def about(request):
+    return render(request, 'rango/about.html')
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")
 
 
